f62cae05f12329b6f988dcaba32b11f75e6e4521
Vadim Rumyantsev <warum48@gmail.com>
2023-07-02 13:15:55 +0700

    inner comments

--- src/App.tsx
+++ src/App.tsx
@@ -163,23 +163,7 @@ export function App() {
       >
         <Outlet />
       </Box>
-      <Box
-        // mx="xl"
-        ml="-1px"
-        pl="1px"
-        my="-0.125rem"
-        sx={{
-          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-          boxShadow: 'inset 0px 4px 3px rgba(50, 50, 50, 0.25)',
-          borderTop: '1px solid #202020',
-        }}
-      >
-        <Box px="xl" pt="xl">
-          <i>Здесь вы можете комментировать процесс разработки страницы:</i>
-
-          <FastCommentsCommentWidget tenantId="2Nf0TURX_Hp" key={'comm' + theme.colorScheme} />
-        </Box>
-      </Box>
+      
     </AppShell>
   );
 }
--- src/_routes/Auth.tsx
+++ src/_routes/Auth.tsx
@@ -14,10 +14,14 @@ import {
     Container,
     Group,
     Button,
+    Box,
+    useMantineTheme,
   } from '@mantine/core';
   
   export function Auth() { //enticationTitle
+    const theme = useMantineTheme();
     return (
+        <>
       <Container size={420} my={40}>
         <Title
           align="center"
@@ -46,5 +50,23 @@ import {
           </Button>
         </Paper>
       </Container>
+      <Box
+        // mx="xl"
+        ml="-1px"
+        pl="1px"
+        my="-0.125rem"
+        sx={{
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+          boxShadow: 'inset 0px 4px 3px rgba(50, 50, 50, 0.25)',
+          borderTop: '1px solid #202020',
+        }}
+      >
+        <Box px="xl" pt="xl">
+          <i>Здесь вы можете комментировать процесс разработки страницы:</i>
+
+          <FastCommentsCommentWidget tenantId="2Nf0TURX_Hp" key={'comm' + theme.colorScheme} />
+        </Box>
+      </Box>
+      </>
     );
   }
